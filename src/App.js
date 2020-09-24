85ccb46f67c4f4528b71b52ca3600b02d93bc9fd
leilafreitas <leilafreitas159@gmail.com>
2020-09-24 20:07:48 -0300

    atualizaçoes

--- src/App.js
+++ src/App.js
@@ -43,6 +43,8 @@ export default()=>{
           {chatList.map((item,key)=>(
             <ChatListItem
               key={key}
+              data={item}
+              active={activeChat.chatId===item.chatId}
               onClick={()=>setActiveChat(item)}
             />
           ))}
--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,9 +1,55 @@
 import React from 'react';
 import './ChatWindow.css';
+import SearchIcon from '@material-ui/icons/Search';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
 export default()=>{
     return(
         <div className='chatWindow'>
-            ...
+            <div className="chatWindow--header">
+                <div className="chatWindow--headerinfo">
+                    <img src='https://www.w3schools.com/howto/img_avatar2.png'/>
+                    <div className='chatWindow--name'>Leila Freitas</div>
+                </div>
+                <div className='chatWindow--headerbuttons'>
+                    <div className="chatWindow--btn">
+                        <SearchIcon style={{color:'#919191'}}/>
+                    </div>
+                    <div className="chatWindow--btn">
+                        <AttachFileIcon style={{color:'#919191'}}/>
+                    </div>
+                    <div className="chatWindow--btn">
+                        <MoreVertIcon style={{color:'#919191'}}/>
+                    </div>
+
+                </div>
+            </div>
+            <div className="chatWindow--body">
+                
+            </div>
+            <div className="chatWindow--footer">
+                <div className="chatWindow-pre">
+                    <div className="chatWindow--btn">
+                        <CloseIcon style={{color:'#919191'}}/>
+                    </div>
+                    <div className="chatWindow--btn">
+                        <InsertEmoticonIcon style={{color:'#919191'}}/>
+                    </div>
+                </div>
+                <div className="chatWindow-inputarea">
+                    <input text="text"
+                    placeholder="Digite uma mensagem"/>
+                </div>
+                <div className="chatWindow-pos">
+                    <div className="chatWindow--btn">
+                        <SendIcon style={{color:'#919191'}}/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }

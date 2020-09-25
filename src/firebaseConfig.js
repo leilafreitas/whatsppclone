1bf2fc3fe19b60798942b14b047034c9a237eb20
leilafreitas <leilafreitas159@gmail.com>
2020-09-25 01:15:24 -0300

    configFirebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyC2xo2c3UysVVL08YLAYpvsa9kDNXQpw08",
+    authDomain: "whatsappjs-6b89f.firebaseapp.com",
+    databaseURL: "https://whatsappjs-6b89f.firebaseio.com",
+    projectId: "whatsappjs-6b89f",
+    storageBucket: "whatsappjs-6b89f.appspot.com",
+    messagingSenderId: "927712537797",
+    appId: "1:927712537797:web:c2d5e379bd8b03bfbfd4ac"
+  };
